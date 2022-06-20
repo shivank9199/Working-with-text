0c75ee1f356aa5632d5ec4cd68f523cd340d11a2
Shivank <shivank.kumar.bce20@itbhu.ac.in>
2022-06-20 20:31:39 +0530

    Commiting after adding dark mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
+import React, { useState } from 'react';
 import './App.css';
-import TextForm from './components/form.js';
+import Alert from './components/Alert';
+import TextForm from './components/Form.js';
+import Navbar from './components/Navbar';
 
 function App() {
+
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message,type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000)
+  }
+
+  const [mode, setMode] = useState('light');
+  const toggle = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#051f39';
+      document.body.style.color = 'white';
+      showAlert('Dark mode enabled', 'success');
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert('Light mode enabled', 'success');
+    }
+  }
+
+  const toggle1 = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white';
+      showAlert('Black mode enabled', 'success');
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert('Light mode enabled', 'success');
+    }
+  }
+
   return (
     <div>
-      <TextForm/>;
+      <Navbar mode={mode} toggleMode={toggle} toggle1Mode={toggle1}/>
+      <Alert alert={alert}/>
+      <TextForm showAlert={showAlert} mode={mode}/>;
     </div>
   );
 }
--- src/components/form.js
+++ src/components/form.js
@@ -1,73 +1,53 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import App from '../App';
 
-export default function TextForm() {
+export default function TextForm(props) {
 
-  
+  const [text, setValue] = useState('');
   const handleUpButton = () => {
     let newText = text.toUpperCase();
     setValue(newText);
+    props.showAlert('Converted to uppercase', 'success')
   }
 
   const handleLowButton = () => {
     let newText = text.toLowerCase();
     setValue(newText);
-  }
-
-  const [darknwhite, setDark] = useState('Dark Mode')
-
-  const handleDarkButton = () => {
-    if (darknwhite === 'Dark Mode') {
-      setDark('White Mode');
-      const note = document.querySelector('.all');
-      note.style.backgroundColor = 'black';
-      note.style.color = 'white';
-    }
-
-    else {
-      setDark('Dark Mode');
-      const note = document.querySelector('.all');
-      note.style.backgroundColor = 'white';
-      note.style.color = 'black';
-    }
+    props.showAlert('Converted to lowercase', 'success')
   }
 
   const textAgain = (event) => {
     setValue(event.target.value);
+    props.showAlert('Text added', 'success')
   }
 
-  const [text, setValue] = useState('');
-
   return (
-
-    <div className='all'>
 
       <div className="mb-3">
           
           <h1>Let's work with the text</h1>
 
           <div className='text-ar'>
-            <textarea placeholder='Enter your text here...' className="form-control" value={text} onChange={textAgain} id="exampleFormControlTextarea1"></textarea>
+            <textarea placeholder='Enter your text here...' className="form-control" style={{backgroundColor: props.mode==='light'?'white':'black', color: props.mode==='light'?'black':'white'}} value={text} onChange={textAgain} id="exampleFormControlTextarea1"></textarea>
           </div>
 
           <div className='extra'>
-            <button className='btn' onClick={handleUpButton}>Change to uppercase</button>
-            <button className='btn' onClick={handleLowButton}>Change to lowercase</button>
-            <button className='btn' onClick={handleDarkButton}>{darknwhite}</button>
+            <button className='btn' style={{backgroundColor: props.mode==='light'?'#22bca5':'#22bca5', color: props.mode==='light'?'black':'black', marginTop: 5, marginRight: 4}} onClick={handleUpButton}>Change to Uppercase</button>
+            <button className='btn' style={{backgroundColor: props.mode==='light'?'#22bca5':'#22bca5', color: props.mode==='light'?'black':'black', marginTop: 5, marginRight: 4}} onClick={handleLowButton}>Change to lowerCase</button>
           </div>
 
-          <div className='extra'>
-            <h2>Info</h2>
+          <div className='extra' style={{marginTop: 10}}>
+            <h3>Summary</h3>
             <p>Your number of words are <b>{text.split(" ").length}</b> and number of characters are <b>{text.length}</b></p>
             <p>It will take <b>{0.2* (text.split(" ").length)} seconds</b> to read</p>
           </div>
 
           <div className='extra'>
-            <h2>Text</h2>
-            <p>{text}</p>
+            <h3>Preview</h3>
+            <p>{text.length>0?text:'Enter some text to see summary'}</p>
           </div>
           
       </div>
-
-    </div>
   )
 }
